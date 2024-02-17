020c6b681c83f2fd02bb487a78dd82cc2c1756ba
ViniciusRimes <viniciusrimess@gmail.com>
2024-02-16 22:34:04 -0300

    Updated page register

--- frontend/SSParisiTeste/src/components/Messages/Messages.tsx
+++ frontend/SSParisiTeste/src/components/Messages/Messages.tsx
@@ -18,10 +18,9 @@ const Messages = () => {
             }, time);
         })
     })
-    console.log(type)
     return (
         visibility && (
-            <Box sx={{padding: '0.5em', marginTop: '2em', fontWeight: 'bold', backgroundColor: type === 'success' ? '#d4edda' : '#f8d7da', border: type === 'success' ? '1px solid #c3e6cb' : '1px solid #f5c6db', borderRadius: '0.5em'}} >
+            <Box sx={{padding: '0.5em', margin: '2em auto', fontWeight: 'bold', backgroundColor: type === 'success' ? '#d4edda' : type === 'error' ? '#f8d7da' : '#E6F2FF', border: type === 'success' ? '1px solid #c3e6cb' : type === 'error' ? '1px solid #f5c6db': '1px solid #4DA8DA', borderRadius: '0.5em', width: message.length - 50 ? '100%' : '70%'}} >
                 <Typography textAlign={'center'}  color={type === 'success' ? '#155724': '#721c24'}>{message}</Typography>
             </Box>
         )
--- frontend/SSParisiTeste/src/components/pages/RegisterClient.tsx
+++ frontend/SSParisiTeste/src/components/pages/RegisterClient.tsx
@@ -1,13 +1,14 @@
+
+
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
-import TextField from '@mui/material/TextField'
 import FormControl from '@mui/material/FormControl'
-import FormLabel from '@mui/material/FormLabel'
 import {Button} from '@mui/material'
 import {ChangeEvent, useState, useEffect } from 'react'
 import api from '../../helpers/axios'
 import Messages from '../Messages/Messages'
 import useFlashMessages from '../../hooks/useFlashMessages'
+import InputPersonzalized from '../Input/InputPersonalized'
 
 interface ApiResponse{
     message: string
@@ -23,21 +24,71 @@ interface ApiError{
 
 const RegisterClient = () => {
     const [cnpj, setCnpj] = useState('')
+    const [cnpjIsValid, setCnpjIsValid] = useState(false)
     const [name, setName] = useState('')
     const [client, setClient] = useState({})
     const [formData, setFormData] = useState({
         name: '',
         cnpj: ''
     })
+    const [address, setAddress] = useState({
+        uf: "",
+        cep: "",
+        municipio: "",
+        bairro: "",
+        numero: "",
+        complemento: ""
+    })
     const {setFlashMessages} = useFlashMessages()
-    
+
     useEffect(()=>{
         setClient({
             name: name, 
-            cnpj: cnpj
+            cnpj: cnpj,
+            address: address
         })
-    }, [cnpj, name])
-    
+    }, [cnpj, name, address])
+    useEffect(()=>{
+        if(cnpj.length === 14){
+            setFlashMessages('Buscando endereço', 'loading', 1499)
+            setTimeout(async() => {
+                try{
+                    const response = await api.get(`/clients/address/${cnpj}`)
+                    if(response.status === 200){
+                        setCnpjIsValid(true)
+                    }
+                    const address = response.data.address
+                    setAddress({
+                        uf: address.uf ? address.uf : "",
+                        cep: address.cep ? address.cep : "",
+                        municipio: address.municipio ? address.municipio : "",
+                        bairro: address.bairro ? address.bairro : "",
+                        numero: address.numero ? address.numero : "",
+                        complemento: address.complemento ? address.complemento : "",
+                    })
+                }catch(error){
+                    if(error instanceof Error && 'response' in error && error.response){
+                        setAddress({
+                            uf: "",
+                            cep: "",
+                            municipio: "",
+                            bairro: "",
+                            numero: "",
+                            complemento: ""
+                        })
+                        const apiError = error as ApiError
+                        setFlashMessages(apiError.response.data.message, 'error', 2500)
+                        setTimeout(() => {
+                            setCnpjIsValid(true)
+                        }, 3000);
+                    }else{
+                        const errorMessage = error as ApiError
+                        setFlashMessages(errorMessage.message, 'error', 1000)
+                    }
+                }
+            }, 1500);
+        }
+    }, [cnpj])
     const handleSubmit = async (e: { preventDefault: () => void })=>{
         e.preventDefault()
         await registerClient()
@@ -51,6 +102,15 @@ const RegisterClient = () => {
                     name: '',
                     cnpj: ''
                 })
+                setAddress({
+                    uf: "",
+                    cep: "",
+                    municipio: "",
+                    bairro: "",
+                    numero: "",
+                    complemento: ""
+                })
+                setCnpjIsValid(false)
             }, 3000);
         }catch(error){
             if(error instanceof Error && 'response' in error && error.response){
@@ -62,10 +122,14 @@ const RegisterClient = () => {
             }
         }
     }
-
+    
     const handleInputChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>): void=>{
         const {value} = e.target
+
         let formattedCnpj: string = value.replace(/\D/g, '')
+        if(formattedCnpj.length !== 14){
+            setCnpjIsValid(false)
+        }
 
         if(formattedCnpj.length > 2){
             formattedCnpj = formattedCnpj.replace(/^(\d{2})(\d)/, '$1.$2')
@@ -96,36 +160,43 @@ const RegisterClient = () => {
 
         }))
     }
+
     return (
-    <Box maxWidth={'600px'} sx={{backgroundColor: '#fff', margin: '0em auto', height: '100vh'}} color={'#000'} width={'100vw'} justifyContent={'space-evenly'} display={'flex'} alignItems={'center'} flexDirection={'column'}>
-        <Typography fontFamily={'Poppins'} variant='h1' fontWeight={'bold'} fontSize={'1.5em'}>Registro de Clientes</Typography>
-        <form style={{display: 'flex', justifyContent: 'center', margin: '0em auto', width: '100%'}} onSubmit={handleSubmit}> 
-        <FormControl sx={{ width: 'calc(100% - 4em)'}}>
-            <FormLabel htmlFor='name'sx={{color: '#6DBDE6', cursor: 'pointer', fontWeight: 'bold', fontFamily:'Poppins'}}>Nome: *</FormLabel>
-            <TextField onChange={(e)=>handleNameChange(e)} value={formData.name} required= {true} placeholder= 'Digite o nome do cliente 'id='name' variant='outlined' sx={{margin: '0.5em 0em' ,fontFamily: 'Poppins', '& input::placeholder': {
-                fontFamily: 'Poppins',
-                opacity: 0.7,
-                fontSize: '0.8em'
-            }}}/>
-            <FormLabel htmlFor='cnpj'sx={{color: '#6DBDE6', cursor: 'pointer', fontWeight: 'bold', fontFamily:'Poppins', marginBottom: '0.5em', marginTop:'0.5em'}}>CNPJ: *</FormLabel>
-            <TextField onChange={(e)=>handleInputChange(e)} required= {true}  placeholder= 'Digite o CNPJ do cliente(somente números) 'id='cnpj' variant='outlined' value={formData.cnpj} inputProps={{maxLength: 18}} sx={{fontFamily: 'Poppins', '& input::placeholder': {
-                fontFamily: 'Poppins',
-                opacity: 0.7,
-                fontSize: '0.8em'
-            }}}/>
+    <Box maxWidth={'900px'} sx={{backgroundColor: '#fff', margin: '3em auto', height: '100vh'}} color={'#000'} width={'100vw'} justifyContent={'center'} display={'flex'} alignItems={'center'} flexDirection={'column'}>
+        <Typography fontFamily={'Poppins'} variant='h1' fontWeight={'bold'} marginBottom={'1em'} fontSize={'1.5em'}>Registro de Clientes</Typography>
+        <Typography  marginTop={'1.5em'} fontFamily={'Poppins'} variant='body1'>Digite os dados abaixo para cadastrar um novo cliente no sistema</Typography>
+        <Typography  marginTop={'1.5em'} fontFamily={'Poppins'} variant='body1'>Após preencher os campos <span style={{fontWeight: 'bold'}}>Nome</span> e <span style={{fontWeight: 'bold'}}>CNPJ</span> abaixo, será buscado os dados de endereço do cliente</Typography>
+        <form style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', margin: '0em auto', marginBottom: '1em', width: '100%'}} onSubmit={handleSubmit}> 
+        <Box sx={{display: 'flex', justifyContent: 'space-between', flexWrap: 'wrap', width: '100%', marginTop: '3em'}}>
+            <InputPersonzalized htmlFor='name' text='Nome *' id='name' handleOnChange={handleNameChange} required={true} placeholder='Digite o nome do cliente' value={formData.name} maxLength={undefined} disabled = {false}/>
+            <InputPersonzalized htmlFor='cnpj' text = 'CNPJ *'  id='cnpj' handleOnChange={handleInputChange} required = {true} placeholder='Digite o CNPJ do cliente' value={formData.cnpj} maxLength={18} disabled = {false}/>
 
-            <Typography fontFamily={'Poppins'} variant='body1' sx={{marginTop: '2em', fontSize: '0.7em', fontWeight: 'bold'}}>* Os campos são obrigatórios</Typography>
+            {cnpjIsValid && 
+                <InputPersonzalized htmlFor='uf' text = 'UF'  id='uf' handleOnChange={handleInputChange} required = {false} placeholder='' value={address.uf} maxLength={undefined} disabled = {cnpj ? false : true}/>}
             
+            {cnpjIsValid && <InputPersonzalized htmlFor='cep' text = 'CEP'  id='cep' handleOnChange={handleInputChange} required = {false} placeholder='' value={address.cep} maxLength={undefined} disabled = {cnpj ? false : true}/>}
+            
+            {cnpjIsValid && <InputPersonzalized htmlFor='municipay' text = 'Município'  id='municipay' handleOnChange={handleInputChange} required = {false} placeholder='' value={address.municipio} maxLength={undefined} disabled = {cnpj ? false : true}/>}
+        
+            {cnpjIsValid && <InputPersonzalized htmlFor='neighborhood' text = 'Bairro'  id='neighborhood' handleOnChange={handleInputChange} required = {false} placeholder='' value={address.bairro} maxLength={undefined} disabled = {cnpj ? false : true}/> }
+            {cnpjIsValid && <InputPersonzalized htmlFor='number' text = 'Número'  id='number' handleOnChange={handleInputChange} required = {false} placeholder='' value={address.numero} maxLength={undefined} disabled = {cnpj ? false : true}/> }
+            {cnpjIsValid&& <InputPersonzalized htmlFor='complemento' text = 'Complemento'  id='complemento' handleOnChange={handleInputChange} required = {false} placeholder='' value={address.complemento} maxLength={undefined} disabled = {cnpj ? false : true}/> }
+        
+        </Box>
+        <Box>
+        <Box marginTop={'0em'} display={'flex'} flexDirection={'column'} alignItems={'center'}>
+            <Typography fontFamily={'Poppins'} variant='body1' sx={{marginTop: '2em', textAlign: 'center', fontSize: '0.7em', fontWeight: 'bold'}}>* Os campos são obrigatórios</Typography>
             <Messages/> {/*Mensagens de sucesso ou erro na tela*/}
-
-            <Button type="submit" sx={{color: '#000',border: '1px solid black', marginTop: '3em', borderRadius: '0.5em', fontFamily: 'Poppins', padding: '0.5em', fontWeight: 'bold',cursor: 'pointer',
+        </Box>
+        <FormControl sx={{display: 'flex', alignItems: 'center'}}>
+            <Button type="submit" sx={{color: '#000',border: '1px solid black', marginBottom: '1em', marginTop: '2em', borderRadius: '0.5em', fontFamily: 'Poppins', padding: '0.7em 1em', fontWeight: 'bold',cursor: 'pointer',
             '&:hover': {
                 backgroundColor: '#6DBDE6',
                 transition: 'all ease 0.2s',
             }}}>Cadastrar</Button>
         </FormControl>
+        </Box>
         </form>
-        
     </Box>
   )
 }
